758046c4e8f10f5e1e7fdc28126302e6860b7f15
Lauren Ybarra <64549039+laurenybarra@users.noreply.github.com>
2021-02-08 17:32:01 -0800

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-let today = document.querySelector(".date");
-let now = new Date();
-let day = now.getDay();
-let month = now.getMonth();
-let date = now.getDate();
-let hour = now.getHours();
-let minute = now.getMinutes();
-
 let days = [
   "Sunday",
   "Monday",
@@ -15,79 +7,81 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
 
+let celTemp = null;
+let city = "Los Angeles";
 let apiKey = "50ea266795ff34ae2a3920026adbb08c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
 
-today.innerHTML = `${days[day]}, ${months[month]} ${date} (${hour}:${
-  (minute < 10 ? "0" : "") + minute
-})`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  return `${days[day]} ${hours}:${minutes}`;
+}
 
-let citySearch = document.querySelector("input");
+function showTemperature(response) {
+  let currentDegree = document.querySelector("#current-degree");
+  let city = document.querySelector(".city");
+  let description = document.querySelector("#description");
+  let windSpeed = document.querySelector("#wind");
+  let humid = document.querySelector("#humidity");
+  let currentDate = document.querySelector(".date");
+  let icon = document.querySelector("#icon");
+  celTemp = response.data.main.temp;
+  currentDegree.innerHTML = `${Math.round(celTemp)}°`;
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  humid.innerHTML = response.data.main.humidity;
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", response.data.weather[0].description);
+}
 
-function enterCity() {
+function enterCity(event) {
   event.preventDefault();
-  let city = document.querySelector("h1");
+  let citySearch = document.querySelector("input");
+  let city = document.querySelector(".city");
   city.innerHTML = citySearch.value;
   let citySearched = citySearch.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=${apiKey}&units=metric`;
-  function showTemperature(response) {
-    let currentDegree = document.querySelector(".current-degree");
-    currentDegree.innerHTML = `${Math.round(response.data.main.temp)}°`;
-  }
   axios.get(`${apiUrl}`).then(showTemperature);
 }
 
 let cityForm = document.querySelector("form");
 cityForm.addEventListener("submit", enterCity);
 
-function degreeCel() {
-  let currentDegree = document.querySelector(".current-degree");
-  currentDegree.innerHTML = "19°";
+function degreeCel(event) {
+  event.preventDefault();
+  let currentDegree = document.querySelector("#current-degree");
+  cel.classList.add("on");
+  fahr.classList.remove("on");
+  currentDegree.innerHTML = `${Math.round(celTemp)}°`;
 }
 
 let cel = document.querySelector("#celsius");
 cel.addEventListener("click", degreeCel);
 
-function degreeFar() {
-  let currentDegree = document.querySelector(".current-degree");
-  currentDegree.innerHTML = "72°";
+function degreeFahr(event) {
+  event.preventDefault();
+  let currentDegree = document.querySelector("#current-degree");
+  cel.classList.remove("on");
+  fahr.classList.add("on");
+  let conversionF = (celTemp * 9) / 5 + 32;
+  currentDegree.innerHTML = `${Math.round(conversionF)}°`;
 }
 
-let far = document.querySelector("#fahrenheit");
-far.addEventListener("click", degreeFar);
-
-function currentLocation() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-function showTemperature(response) {
-  let temp = Math.round(response.data.main.temp);
-  let currentDegree = document.querySelector(".current-degree");
-  let h1 = document.querySelector("h1");
-  currentDegree.innerHTML = `${temp}°`;
-  h1.innerHTML = response.data.name;
-}
-
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
-
-let currentCity = document.querySelector("#current-city");
-currentCity.addEventListener("click", currentLocation);
+let fahr = document.querySelector("#fahrenheit");
+fahr.addEventListener("click", degreeFahr);
